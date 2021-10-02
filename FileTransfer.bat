fefa6a8ce2cc9c3c124a6fa66cca0de73cf75f49
StormFalcon32 <benchen9708@gmail.com>
2021-10-02 17:03:09 -0500

    Update to new ssh key

--- FileTransfer.bat
+++ FileTransfer.bat
@@ -1,3 +1,3 @@
-scp -i Ben.pem discord_bot.py pi@192.168.0.213:
-scp -i Ben.pem sheetskey.json pi@192.168.0.213:
-scp -i Ben.pem token.txt pi@192.168.0.213:
+scp -i C:\Users\bench\.ssh\pi.pem discord_bot.py pi@192.168.0.213:
+scp -i C:\Users\bench\.ssh\pi.pem sheetskey.json pi@192.168.0.213:
+scp -i C:\Users\bench\.ssh\pi.pem token.txt pi@192.168.0.213:
