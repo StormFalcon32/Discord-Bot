b22eb164bf25963cfafc32c3df16ca670538bf25
StormFalcon32 <benchen9708@gmail.com>
2021-10-02 12:30:50 -0500

    Updating scp commands

--- FileTransfer.bat
+++ FileTransfer.bat
@@ -1,3 +1,3 @@
-scp -i Ben.pem DiscordBot.py pi@192.168.1.21:
-scp -i Ben.pem sheetskey.json pi@192.168.1.21:
-scp -i Ben.pem token.txt pi@192.168.1.21:
+scp -i Ben.pem discord_bot.py pi@192.168.0.213:
+scp -i Ben.pem sheetskey.json pi@192.168.0.213:
+scp -i Ben.pem token.txt pi@192.168.0.213:
