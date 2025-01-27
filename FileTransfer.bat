7b82996a4b302767c3ca9275450f752acc1363de
Benjamin Chen <benchen9708@gmail.com>
2025-01-26 20:39:09 -0600

    update to take advantage of ssh config

--- FileTransfer.bat
+++ FileTransfer.bat
@@ -1,3 +1,3 @@
-scp -i C:\Users\bench\.ssh\pi.pem discord_bot.py pi@192.168.0.213:
-scp -i C:\Users\bench\.ssh\pi.pem sheetskey.json pi@192.168.0.213:
-scp -i C:\Users\bench\.ssh\pi.pem token.txt pi@192.168.0.213:
+scp discord_bot.py pi:
+scp sheetskey.json pi:
+scp token.txt pi:
